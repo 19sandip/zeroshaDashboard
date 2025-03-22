c3704c310817327984647df100c30b75df882d04
sandeep kumar <sandeep307098@gmail.com>
2025-03-22 15:06:32 +0530

    this is dashboard of zerodha clone

---
+++ src/components/Dashboard.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Apps from "./Apps";
+import Funds from "./Funds";
+import Holdings from "./Holdings";
+
+import Orders from "./Orders";
+import Positions from "./Positions";
+import Summary from "./Summary";
+import WatchList from "./WatchList";
+import { GeneralContextProvider } from "./GeneralContext";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <GeneralContextProvider>
+        <WatchList />
+      </GeneralContextProvider>
+      <div className="content">
+        <Routes>
+          <Route exact path="/" element={<Summary />} />
+          <Route path="/dashboard/orders" element={<Orders />} />
+          <Route path="/dashboard/holdings" element={<Holdings />} />
+          <Route path="/dashboard/positions" element={<Positions />} />
+          <Route path="/dashboard/funds" element={<Funds />} />
+          <Route path="/dashboard/apps" element={<Apps />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Orders.js
@@ -0,0 +1,63 @@
+import React,{useState, useEffect} from "react";
+// import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Orders = () => {
+  const [allOrders, setAllOrders] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3004/allOrders").then((response) => {
+      setAllOrders(response.data);
+    });
+  }, []);
+
+  // const allOrders = [
+  //   {
+  //     name: "RELIANCE",
+  //     qty: 10,
+  //     price: 2112.4,
+  //     mode: "BUY",
+  //   },
+  //   {
+  //     name: "HUL",
+  //     qty: 20,
+  //     price: 512.4,
+  //     mode: "SELL",
+  //   },
+  // ];
+  return (
+    <div className="order-table">
+    
+        <table>
+        <thead>
+          <tr>
+            
+            <th>Name</th>
+            <th>Qty</th>
+            <th>Price</th>
+            <th>Mode</th>
+        
+          </tr>
+        </thead>
+        <tbody>
+          {allOrders.map((allOrders) => {
+            return (
+               <tr>
+            <td>{allOrders.name}</td>
+            <td>{allOrders.qty}</td>
+            <td>{allOrders.price}</td>
+            <td>{allOrders.mode}</td>
+           
+           
+          </tr>)
+            
+          })}
+         
+
+        </tbody>
+      </table>
+      </div>
+    
+  );
+};
+
+export default Orders;
